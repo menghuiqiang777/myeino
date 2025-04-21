2675b057dfb2a1352c0fa1baf522e238bea59fe3
MENG <menghuiqiang@zxtech.info>
2025-04-21 13:48:40 +0800

    v0.0.12

---
+++ mod.go
@@ -0,0 +1,10 @@
+module github.com/menghuiqiang777/myeino
+
+go 1.24
+require (
+	"github.com/cloudwego/eino/components/model"
+	"github.com/cloudwego/eino/schema"
+	"github.com/cloudwego/eino-ext/components/model/ark"
+	"github.com/cloudwego/eino-ext/components/model/qwen"
+	
+)
